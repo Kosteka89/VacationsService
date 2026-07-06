ed0931940736291ba6eb933de4c894967ebc6978
Konstantin <kosteka89@gmail.com>
2026-07-06 17:05:03 +0300

    Initial commit

---
+++ src/main/java/ru/netology/services/VacationsService.java
@@ -0,0 +1,23 @@
+package ru.netology.services;
+
+public class VacationsService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0;
+        int money = 0;
+        for (int month = 1; month <= 12; month++) {
+            if (money >= threshold) {
+
+                count ++;
+                money = money - expenses;
+                money = month / 3;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
+
+        }
+        return count;
+
+    }
+}
